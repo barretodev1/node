7343e7e5aef5f81c95e8fb6fe640fcf227fb0567
Caio <caiobarreto1acim@gmail.com>
2024-08-05 19:54:22 -0300

    Commitando o Node

--- Middlewares/auth.js
+++ Middlewares/auth.js
@@ -1,24 +1,23 @@
-import jwt from 'jsonwebtoken'
-const JWT_SECRET = process.env.JWT_SECRET
+    import jwt from 'jsonwebtoken'
+    const JWT_SECRET = process.env.JWT_SECRET
 
-const auth = (req, res, next) => {
+    const auth = (req, res, next) => {
 
-    const token = req.headers.authorization
+        const token = req.headers.authorization
 
-    if(!token) {
-        return res.status(401).json({message: 'ACESSO NEGADO'})
+        if(!token) {
+            return res.status(401).json({message: 'ACESSO NEGADO'})
+        }
+
+        try {
+            const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
+            console.log(decoded)
+    
+        } catch (error) {
+            return res.status(401).json({message: 'TOKEN INVÁLIDO'})
+        }
+
+        next()
     }
 
-    try {
-        const decoded = jwt.verify(token.replace('Bearer ', ''), JWT_SECRET)
-
-        console.log(decoded)
-  
-    } catch (error) {
-        return res.status(401).json({message: 'TOKEN INVÁLIDO'})
-    }
-
-    next()
-}
-
-export default auth;
+    export default auth;
--- api.js
+++ api.js
@@ -2,9 +2,12 @@ import express from 'express'
 import meuApp from './Routers/public.js'
 import meuApp2 from './Routers/private.js'
 import auth1 from './Middlewares/auth.js'
+import cors from 'cors'
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(meuApp)
 app.use(auth1, meuApp2)
+
 
 app.listen(3000, () => console.log('SUA API ESTÁ RODANDO'))
